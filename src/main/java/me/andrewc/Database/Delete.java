9f4679b48b51ff905489e069ff42869fd253258e
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-28 00:36:12 +0100

    Edited Drop method with concurrency for better efficiency

--- src/main/java/me/andrewc/Database/Delete.java
+++ src/main/java/me/andrewc/Database/Delete.java
@@ -5,6 +5,9 @@ import org.slf4j.Logger;
 
 import java.io.FileNotFoundException;
 import java.sql.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Delete extends Base {
     private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Delete.class);
@@ -13,34 +16,42 @@ public class Delete extends Base {
     }
 
     public boolean Drop(Class<?> tableName, boolean Cascade) throws SQLException {
-        Connection conn = getConnection();
-        if (conn == null) {
-            logger.error("Could not get connection");
-            throw new SQLException("Could not get connection");
-        }
-        String query = "DROP TABLE IF EXISTS 1;";
-        EntityProcessor.process(tableName);
-        query = query.replace("1", EntityProcessor.getTableName());
-        if (Cascade) {
-            query = query.replace(";", " CASCADE;");
-        }
-        Statement stmt = conn.createStatement();
-        stmt.execute(query);
+        Callable<Boolean> callable = () -> {
+            Connection conn = getConnection();
+            if (conn == null) {
+                logger.error("Could not get connection");
+                throw new SQLException("Could not get connection");
+            }
+            String query = "DROP TABLE IF EXISTS 1;";
+            EntityProcessor.process(tableName);
+            query = query.replace("1", EntityProcessor.getTableName());
+            if (Cascade) {
+                query = query.replace(";", " CASCADE;");
+            }
+            Statement stmt = conn.createStatement();
+            stmt.execute(query);
 
-        logger.info("Dropping table: " + tableName);
+            boolean deleted = false;
+            DatabaseMetaData dbm = conn.getMetaData();
+            ResultSet tables = dbm.getTables(null, "main", EntityProcessor.getTableName(), null);
+            if (tables.next()) {
+                logger.info("Table " + EntityProcessor.getTableName() + " not dropped");
+            } else {
+                logger.warn("Table " + EntityProcessor.getTableName() + " dropped or does not exist");
+                deleted = true;
+            }
+            stmt.close();
+            conn.close();
+            return deleted;
+        };
 
-        boolean deleted = false;
-        DatabaseMetaData dbm = conn.getMetaData();
-        ResultSet tables = dbm.getTables(null, "main", EntityProcessor.getTableName(), null);
-        if (tables.next()) {
-            logger.info("Table " + EntityProcessor.getTableName() + " not dropped");
-        } else {
-            logger.warn("Table " + EntityProcessor.getTableName() + " dropped");
-            deleted = true;
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        try {
+            return executor.submit(callable).get();
+        } catch (Exception ex) {
+            logger.error("Failed to drop table: " + tableName.getName());
+            return false;
         }
-        stmt.close();
-        conn.close();
-        return deleted;
     }
 
     public boolean DeleteFrom(Class<?> table, String where, String condition) throws SQLException {
