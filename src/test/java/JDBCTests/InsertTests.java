e3766822ad1b4803da5d47255fbb2ee1294f1e31
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-28 00:21:53 +0100

    Added test case for multiple row insertion

--- src/test/java/JDBCTests/InsertTests.java
+++ src/test/java/JDBCTests/InsertTests.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import testModels.Book;
 
@@ -25,11 +27,29 @@ public class InsertTests {
     }
 
     @Test
-    public void insert_single_success() throws SQLException, FileNotFoundException, NoFieldFound, InvalidValueLength, InvalidTypeException, IllegalAccessException {
+    public void insert_single_success() throws SQLException, FileNotFoundException {
         Book book = new Book("The Hobbit", new BigDecimal("12.99"), "A book about a hobbit", 300);
         Insert insert = new Insert("test.properties");
         boolean success = insert.InsertSingle(Book.class, book);
 
+        assertTrue(success);
+    }
+
+    @Test
+    public void insert_multiple_success() throws SQLException, FileNotFoundException {
+        Book book = new Book("The Hobbit", new BigDecimal("12.99"), "A book about a hobbit", 300);
+        Book book2 = new Book("The Hobbit 2", new BigDecimal("12.99"), "A book about a hobbit", 300);
+        Book book3 = new Book("The Hobbit 3", new BigDecimal("12.99"), "A book about a hobbit", 300);
+        Book book4 = new Book("The Hobbit 4", new BigDecimal("12.99"), "A book about a hobbit", 300);
+
+        List<Book> books = new ArrayList<>();
+        books.add(book);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+
+        Insert insert = new Insert("test.properties");
+        boolean success = insert.InsertMultiple(Book.class, books.toArray());
         assertTrue(success);
     }
 }
