ca9b5bf6a0a9329b13f2a0d094b6e5ebb79f73d7
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-27 17:23:45 +0100

    Added base processor class for DateTime

---
+++ src/main/java/me/andrewc/Processors/DateTimeProcessor.java
@@ -0,0 +1,10 @@
+package me.andrewc.Processors;
+
+public class DateTimeProcessor {
+
+    public static String format;
+
+    public static boolean process(Object obj) {
+        return true;
+    }
+}
