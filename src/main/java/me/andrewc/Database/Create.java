76b9e41ac98b0bb9563651708906a9aee4b1caba
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-28 00:47:46 +0100

    Made CreateTable method concurrent

--- src/main/java/me/andrewc/Database/Create.java
+++ src/main/java/me/andrewc/Database/Create.java
@@ -1,9 +1,5 @@
 package me.andrewc.Database;
 
-
-import me.andrewc.Exceptions.InvalidTypeException;
-import me.andrewc.Exceptions.InvalidValueLength;
-import me.andrewc.Exceptions.NoFieldFound;
 import me.andrewc.Processors.EntityProcessor;
 import me.andrewc.Processors.ColumnProcessor;
 import org.slf4j.Logger;
@@ -11,6 +7,7 @@ import org.slf4j.Logger;
 import java.io.FileNotFoundException;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.concurrent.Callable;
 
 public class Create extends Base {
     private static final Logger logger = org.slf4j.LoggerFactory.getLogger(Create.class);
@@ -19,62 +16,71 @@ public class Create extends Base {
         super(filePath);
     }
 
-    public boolean CreateTable(Class<?> clazz) throws SQLException, NoFieldFound, InvalidValueLength, InvalidTypeException {
-        // Get connection
-        Connection conn = getConnection();
-        if (conn == null) {
-            logger.error("Could not get connection");
-            throw new SQLException("Could not get connection");
-        }
-        // Create Statement
-        String query = "CREATE TABLE IF NOT EXISTS 1 (2);";
-        // Replace 1 with table name
-        EntityProcessor.process(clazz);
-        query = query.replace("1", EntityProcessor.getTableName());
-        // Now insert column names
-        ColumnProcessor.process(clazz);
-        ArrayList<String> columns = ColumnProcessor.getColumns();
-        // Now get the column types
-        ArrayList<String> types = ColumnProcessor.getTypes();
-        // Execute Statement
-        ArrayList<String> constraints = ColumnProcessor.getConstraints();
-
-        // Construct each column with its type and constraints
-        ArrayList<String> lines = new ArrayList<>();
-
-        StringBuilder line = new StringBuilder();
-        for (int x = 0; x < columns.size(); x++) {
-            line.append(columns.get(x));
-            line.append(" ");
-            line.append(types.get(x));
-            if (constraints.size() > 0) {
-                line.append(" ");
-                line.append(constraints.get(x));
+    public boolean CreateTable(Class<?> clazz) {
+        Callable<Boolean> callable = () -> {
+            // Get connection
+            Connection conn = getConnection();
+            if (conn == null) {
+                logger.error("Could not get connection");
+                throw new SQLException("Could not get connection");
             }
-            lines.add(line.toString());
-            line = new StringBuilder();
+            // Create Statement
+            String query = "CREATE TABLE IF NOT EXISTS 1 (2);";
+            // Replace 1 with table name
+            EntityProcessor.process(clazz);
+            query = query.replace("1", EntityProcessor.getTableName());
+            // Now insert column names
+            ColumnProcessor.process(clazz);
+            ArrayList<String> columns = ColumnProcessor.getColumns();
+            // Now get the column types
+            ArrayList<String> types = ColumnProcessor.getTypes();
+            // Execute Statement
+            ArrayList<String> constraints = ColumnProcessor.getConstraints();
+
+            // Construct each column with its type and constraints
+            ArrayList<String> lines = new ArrayList<>();
+
+            StringBuilder line = new StringBuilder();
+            for (int x = 0; x < columns.size(); x++) {
+                line.append(columns.get(x));
+                line.append(" ");
+                line.append(types.get(x));
+                if (constraints.size() > 0) {
+                    line.append(" ");
+                    line.append(constraints.get(x));
+                }
+                lines.add(line.toString());
+                line = new StringBuilder();
+            }
+
+            // Now construct the query, (replace 2 with the generated lines)
+            query = query.replace("2", String.join(", ", lines));
+            PreparedStatement stmt = conn.prepareStatement(query);
+
+            // Execute the query
+            stmt.execute();
+
+            // Check if table was created
+            boolean created = false;
+            DatabaseMetaData dbm = conn.getMetaData();
+            ResultSet tables = dbm.getTables(null, "main", EntityProcessor.getTableName(), null);
+            if (tables.next()) {
+                logger.info("Table " + EntityProcessor.getTableName() + " created");
+                created = true;
+            } else {
+                logger.warn("Table " + EntityProcessor.getTableName() + " not created");
+            }
+            // Close connection and Statement
+            stmt.close();
+            conn.close();
+            return created;
+        };
+
+        try {
+            return callable.call();
+        } catch (Exception ex) {
+            logger.error("Failed to create table: " + clazz.getName());
+            return false;
         }
-
-        // Now construct the query, (replace 2 with the generated lines)
-        query = query.replace("2", String.join(", ", lines));
-        PreparedStatement stmt = conn.prepareStatement(query);
-
-        // Execute the query
-        stmt.execute();
-
-        // Check if table was created
-        boolean created = false;
-        DatabaseMetaData dbm = conn.getMetaData();
-        ResultSet tables = dbm.getTables(null, "main", EntityProcessor.getTableName(), null);
-        if (tables.next()) {
-            logger.info("Table " + EntityProcessor.getTableName() + " created");
-            created = true;
-        } else {
-            logger.warn("Table " + EntityProcessor.getTableName() + " not created");
-        }
-        // Close connection and Statement
-        stmt.close();
-        conn.close();
-        return created;
     }
 }
